fca6d775a5b6ead627a1f92fcb3ee84b4be525af
monitkhale3-prog <monitkhale3@gmail.com>
2025-09-21 03:10:37 -0700

    Create server.py
    server for back end dev

---
+++ server.py
@@ -0,0 +1,9 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return send_from_directory("home.html")
+
+if __name__ == '__main__':
+   app.run(debug=True)
