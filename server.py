ac2af56e4a838bbce0ccae407f5bfe5137559e56
monitkhale3-prog <monitkhale3@gmail.com>
2025-09-21 03:15:57 -0700

    Update server.py

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from flask import Flask, send_from_directory
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return send_from_directory("home.html")
+    return send_from_directory("index.html")
 
 if __name__ == '__main__':
    app.run(debug=True)
